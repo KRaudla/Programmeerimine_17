35c80f47053777c7c49d47d184d6af3752edf0fa
KRaudla <kasparraudla@gmail.com>
2017-10-23 14:47:04 +0300

    parse input, string to int

---
+++ src/01_exercises/04-Parsing-input/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Parsing_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimene number:");
+            string firstString = Console.ReadLine();
+            // string to number
+            int firstInt = int.Parse(firstString);
+            Console.WriteLine("Sisesta teine number:");
+            string secondString = Console.ReadLine();
+            // string to number
+            int secondInt = int.Parse(secondString);
+
+            Console.WriteLine(firstInt + secondInt);
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
