93244d44e42e64965003d3fbb95c6a7681fab786
KRaudla <kasparraudla@gmail.com>
2017-10-23 15:26:23 +0300

    ask input while true

---
+++ src/01_exercises/06-guess-number-while-true/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_guess_number_while_true
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //generate random number between 0-100
+            Random rndGenerator = new Random();
+            int gnrNumber = rndGenerator.Next(1, 101);
+            while (true)
+            {
+                Console.WriteLine("Paku üks number vahemikus 0-100?");
+                //ask user input
+                string input = Console.ReadLine();
+
+                //Parse user input
+                int userInput = int.Parse(input);
+
+                if (userInput<gnrNumber)
+                {
+                    Console.WriteLine("Sinu number on väiksem");
+                }
+                if (userInput > gnrNumber)
+                {
+                    Console.WriteLine("Sinu number on suurem");
+                }
+                if (userInput == gnrNumber)
+                {
+                    Console.WriteLine("bingo");
+                    break;
+                }
+                
+                //Console.ReadKey();
+
+
+            }
+
+
+        }
+    }
+}
