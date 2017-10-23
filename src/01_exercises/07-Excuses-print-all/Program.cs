c8a699f555bf4cfeb25d01b5e8ec74da471ced1d
KRaudla <kasparraudla@gmail.com>
2017-10-23 15:34:57 +0300

    print array values

---
+++ src/01_exercises/07-Excuses-print-all/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Excuses_print_all
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excuses = new[] { "esimene", "teine", "kolmas", "neljas" };
+            for (int i = 0; i < excuses.Length; i++) 
+            {
+                Console.WriteLine($"{i + 1}. {excuses[i]}");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
