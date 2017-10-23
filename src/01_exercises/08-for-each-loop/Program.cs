4563b3f8aa653c273d7d4f4ca08c06d0e1e9fab0
KRaudla <kasparraudla@gmail.com>
2017-10-23 15:47:41 +0300

    for each loop

---
+++ src/01_exercises/08-for-each-loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_for_each_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] excuses = new[] { "esimene", "teine", "kolmas", "neljas" };
+
+            int eCnt = 0;
+            foreach(string element in excuses)
+            {
+
+                Console.WriteLine($"{eCnt}".);
+                eCnt++;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
