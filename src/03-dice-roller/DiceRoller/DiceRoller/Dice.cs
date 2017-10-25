992df311f542aed99d2d77bbe7c450f9d5a04c3d
KRaudla <kasparraudla@gmail.com>
2017-10-25 15:06:24 +0300

    dice description added

---
+++ src/03-dice-roller/DiceRoller/DiceRoller/Dice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    public class Dice
+    {
+        //private static Random rnd = new Random();
+        private Random Random { get; }
+        private int Sides { get; }
+        public string Desc { get; }
+
+        public int Roll()
+        {
+            return Random.Next(1,this.Sides+1);
+
+        }
+
+        //private readonly Random random;
+        public Dice(int sides, String desc)
+        {
+            this.Desc = desc;
+            this.Sides = sides;
+            this.Random = new Random();
+        }
+    }
+}
---
+++ src/03-dice-roller/DiceRoller/DiceRoller/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("> /roll 3d6 2d8");
+            Console.WriteLine();
+
+            //Dice dice = new Dice();//created class "Dice.cs"  and object
+            Dice d6 = new Dice(6,"d6");//input sides '6' and description 'd6'
+            Dice d8 = new Dice(8,"d8");
+
+            int sum = 0;//collecting values from loops
+
+            for (int i = 0; i < 3; i++)
+            {
+                int diceVal = d6.Roll();
+                Console.WriteLine($"1{d6.Desc} {diceVal}");
+                sum += diceVal;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                int diceVal = d8.Roll();
+                Console.WriteLine($"1{d8.Desc} {diceVal}");
+                sum += diceVal;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Total: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
