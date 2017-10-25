bc748dadebea60c9d6dc742932772f57606a45f5
KRaudla <kasparraudla@gmail.com>
2017-10-25 12:49:01 +0300

    calculates discount percent

---
+++ src/02-graded-assignment/GradedAssignment/GradedAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradedAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a discount calculator"+"\n"+"Please enter the amount: " );
+            Console.ReadLine();
+
+
+
+
+           
+        }
+    }
+}
