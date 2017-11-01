b89ee5539a8561b336fe74aa196255cabb586967
KRaudla <kasparraudla@gmail.com>
2017-11-01 17:22:29 +0200

    rolling some dices again

---
+++ src/03-dice-roller/DiceRoller2/DiceRoller2/Dice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller2
+{
+    class Dice
+    {
+        //class variables
+        public int Sides { get; }
+        public string Desc { get; }
+
+        //lets define the dice constructor
+        public Dice (int sides, string desc)
+        {
+            this.Sides = sides;
+            this.Desc = desc;
+        }
+
+        //Lets define some default dices, with sides 6 and 8.
+        public static Dice D6 = new Dice(6, "db6");
+        public static Dice D8 = new Dice(8, "db8");
+
+        //make random for roll method
+        private static Random Rnd { get; } = new Random();
+        // roll method to return randomly generated number
+        public int Roll()
+        {
+            return Dice.Rnd.Next(1, this.Sides + 1);
+        }
+
+    }
+}
---
+++ src/03-dice-roller/DiceRoller2/DiceRoller2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("> Rolling some Dices...");
+            Console.WriteLine();//blank line 
+
+            //diceroller object
+            DiceRoller diceRoller = new DiceRoller();
+            //make list of dices
+            List<Dice> dices = new List<Dice> {Dice.D6,Dice.D6,Dice.D6,Dice.D8,Dice.D8 };
+            //roll the dices to get results
+            List<DiceRoll> results = diceRoller.Roll(dices);
+            //echo results
+            foreach(var r in results)
+            {
+                Console.WriteLine($"1{ r.Description}, { r.Value}");
+            }
+            //sum the dice values
+            Console.WriteLine($"Sum: {results.Sum(x => x.Value)}");
+
+            Console.WriteLine(">");
+            Console.ReadLine();
+
+        }
+    }
+}
