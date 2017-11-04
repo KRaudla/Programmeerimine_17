9708f082b7ef9e994672a386abefdda9a1ca6d62
KRaudla <kasparraudla@gmail.com>
2017-11-04 11:50:12 +0200

    fixed dice descriptions

--- src/03-dice-roller/DiceRoller2/DiceRoller2/Dice.cs
+++ src/03-dice-roller/DiceRoller2/DiceRoller2/Dice.cs
@@ -20,15 +20,15 @@ namespace DiceRoller2
         }
 
         //Lets define some default dices
-        public static Dice D4 = new Dice(4, "db4");
-        public static Dice D6 = new Dice(6, "db6");
-        public static Dice D8 = new Dice(8, "db8");
-        public static Dice D10 = new Dice(10, "db10");
-        public static Dice D12 = new Dice(12, "db12");
-        public static Dice D14 = new Dice(14, "db14");
-        public static Dice D16 = new Dice(16, "db16");
-        public static Dice D18 = new Dice(18, "db18");
-        public static Dice D20 = new Dice(20, "db20");
+        public static Dice D4 = new Dice(4, "d4");
+        public static Dice D6 = new Dice(6, "d6");
+        public static Dice D8 = new Dice(8, "d8");
+        public static Dice D10 = new Dice(10, "d10");
+        public static Dice D12 = new Dice(12, "d12");
+        public static Dice D14 = new Dice(14, "d14");
+        public static Dice D16 = new Dice(16, "d16");
+        public static Dice D18 = new Dice(18, "d18");
+        public static Dice D20 = new Dice(20, "d20");
 
         //make random for roll method
         private static Random Rnd { get; } = new Random();
