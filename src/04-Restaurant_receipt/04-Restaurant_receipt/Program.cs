157d03f4c09d680b5efb7336c8850a126853df94
KRaudla <kasparraudla@gmail.com>
2017-11-20 14:32:18 +0200

    output encoding and 2 decimal places

--- src/04-Restaurant_receipt/04-Restaurant_receipt/Program.cs
+++ src/04-Restaurant_receipt/04-Restaurant_receipt/Program.cs
@@ -10,7 +10,7 @@ namespace _04_Restaurant_receipt
     {
         static void Main(string[] args)
         {
-
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Taco Palenque");
             Console.WriteLine("1200 Main ST.");
 
@@ -18,7 +18,7 @@ namespace _04_Restaurant_receipt
             
             while (true)
             {
-                Console.Write("Enter price of food item [-i to quit]:");
+                Console.Write("Enter price of food item [-1 to quit]:");
                 Console.WriteLine();
                 float input = float.Parse(Console.ReadLine());
                 
@@ -33,8 +33,8 @@ namespace _04_Restaurant_receipt
 
             var total = items.Sum(x => x.Value);
             Console.WriteLine($"Subtotal: {total}");
-            Console.WriteLine($"15.00 % Graduity:{total*0.15}");
-            Console.WriteLine($"Total: €{total-total*0.15}");
+            Console.WriteLine($"15.00 % Graduity:{total*0.15:F2}");
+            Console.WriteLine($"Total: €{total-total*0.15:F2}");
             Console.WriteLine("Press any key to continue");
             Console.ReadLine();
         }
