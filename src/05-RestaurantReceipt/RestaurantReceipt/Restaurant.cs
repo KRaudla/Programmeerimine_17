2ce36ebd72bb4bed8b3d55555e30fbce9dcba43d
KRaudla <kasparraudla@gmail.com>
2017-11-20 17:40:50 +0200

    Second assignement

---
+++ src/05-RestaurantReceipt/RestaurantReceipt/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReceipt
+{
+    class Restaurant
+    {
+
+        public string Name { get; }
+        public string Street { get; }
+
+        public Restaurant(string name, string street)
+        {
+            this.Name = name;
+            this.Street = street;
+        }
+
+        public Receipt GetReceipt(Tab tab)
+        {
+            return new Receipt(this.Name, this.Street, tab);
+        }
+    }
+}
