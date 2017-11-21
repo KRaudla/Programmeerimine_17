91b7e14bbcfc3c8fc390e3b92313e059e7186c03
KRaudla <kasparraudla@gmail.com>
2017-11-21 10:48:25 +0200

    console prints out the logic to build blackjack

--- src/06-blackjack/Blackjack/Blackjack.Client.Console/Program.cs
+++ src/06-blackjack/Blackjack/Blackjack.Client.Console/Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Blackjack.Client.Console
+﻿namespace Blackjack.Client.Console
 {
-    class Program
+    using System;
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the game of Blackjack");
+            Console.WriteLine();
+
+            Console.WriteLine("You have been dealt: 4C, 6H");
+            Console.WriteLine("You have been dealt: 8S, [?]");
+
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("Choose 1 - take antoher card");
+            Console.WriteLine("Choose 2 - finish");
+
+            Console.WriteLine();
+            Console.WriteLine("I Choose 1");
+            Console.WriteLine("You have been dealt 4S");
+            Console.WriteLine("House has been dealt [?]");
+            Console.WriteLine();
+
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("Choose 1 - take antoher card");
+            Console.WriteLine("Choose 2 - finish");
+
+            Console.WriteLine();
+            Console.WriteLine("I Choose 1");
+            Console.WriteLine("You have been dealt 4S");
+            Console.WriteLine("House has been dealt [?]");
+            Console.WriteLine();
+
+            Console.WriteLine("Your total: 17");
+            Console.WriteLine("House has total: 21");
+
+
+
+
+
+
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            
+            
+
         }
     }
 }
